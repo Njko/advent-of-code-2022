42626472d793bec590f39d4dbbf1967dc8fa2c2c
Nicolas LND <nicolas.linard@gmail.com>
2022-12-03 09:54:23 +0100

    feat: Day 03 challenge complete

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -3,22 +3,50 @@ package day03
 
 import readInput
 
+const val ASCII_LOWER_CASE_SHIFT = 96
+const val ASCII_UPPER_CASE_SHIFT = 64
+const val LOWER_UPPERCASE_OFFSET = 26
 fun main() {
+    fun getItemPriority(equalItem: Char) = if (equalItem.isLowerCase()) {
+        equalItem.code - ASCII_LOWER_CASE_SHIFT
+    } else {
+        equalItem.code - ASCII_UPPER_CASE_SHIFT + LOWER_UPPERCASE_OFFSET
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { rucksack ->
+            val compartments = rucksack.chunked(rucksack.length / 2)
+
+            val equalItem = compartments[0]
+                .firstNotNullOf { item ->
+                    item.takeIf {
+                        compartments[1].contains(item)
+                    }
+                }
+
+            getItemPriority(equalItem)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.chunked(3)
+            .map { group ->
+                group[0].firstNotNullOf { item ->
+                    item.takeIf {
+                        group[1].contains(item) && group[2].contains(item)
+                    }
+                }
+            }
+            .sumOf { getItemPriority(it) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     println("Test results:")
     println(part1(testInput))
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 157)
     println(part2(testInput))
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println("Final results:")
---
+++ src/day04/Day04.kt
@@ -0,0 +1,27 @@
+// ktlint-disable filename
+package day04
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    println("Test results:")
+    println(part1(testInput))
+    check(part1(testInput) == 0)
+    println(part2(testInput))
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day04")
+    println("Final results:")
+    println(part1(input))
+    println(part2(input))
+}
