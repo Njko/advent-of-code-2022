7c03f2b6a84ea173f857ab4771c6caca05e76128
Nicolas LND <nicolas.linard@gmail.com>
2022-12-19 17:43:03 +0100

    feat: Day 17 challenge not complete

---
+++ src/exercises/Day17.kt
@@ -0,0 +1,29 @@
+// ktlint-disable filename
+package exercises
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day17_test")
+    println("Test results:")
+    println(part1(testInput))
+    check(part1(testInput) == 0)
+    println(part2(testInput))
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day17")
+    println("Final results:")
+    println(part1(input))
+    check(part1(input) == 0)
+    println(part2(input))
+    check(part2(input) == 0)
+}
